2bb05764567628d05b0a3d0c1b4fc83112d56630
nalifc <adamnuralifi11@gmail.com>
2023-09-12 07:41:03 +0700

    coba array

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        String[] usernmae = new String[100];
+        String[] password = new String[100];
+
     }
 }
