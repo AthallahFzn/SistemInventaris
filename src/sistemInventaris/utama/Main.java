8aa89fde348a59cef8d7ca58b37d22f7c37841fa
nalifc <adamnuralifi11@gmail.com>
2023-10-24 09:06:48 +0700

    Update

--- src/sistemInventaris/utama/Main.java
+++ src/sistemInventaris/utama/Main.java
@@ -11,10 +11,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         String[][] users = new String[100][2];
-        String[][] items = new String[100][3];
+        String[][] items = new String[100][4];
+        // int[][] items2 = new int[100][2];
 
         int userCount = 0;
         int itemcount = 0;
+        // int itemcount2 = 0;
         boolean sudahMasuk = false;
         // System.out.println(String.format("%d", items[1][2]));
         while (true) {
@@ -23,9 +25,10 @@ public class Main {
             System.out.println("|-----------------------|");
             System.out.println("| 1. Daftar             |");
             System.out.println("| 2. Masuk              |");
-            System.out.println("| 3. Keluar             |");
+            System.out.println("| 3. Input Data         |");
+            System.out.println("| 4. Keluar             |");
             System.out.println("|-----------------------|");
-            System.out.print("Pilih 1-3 : ");
+            System.out.print("Pilih 1-4 : ");
             int pilih = input.nextInt();
             input.nextLine();
             switch (pilih) {
@@ -57,6 +60,39 @@ public class Main {
                     break;
                 case 3:
                     if (sudahMasuk) {
+                        System.out.print("Masukkan Jumlah Barang yang akan diinput : ");
+                        int jml = input.nextInt();
+                        for (int i = 0; i < jml; i++) {
+                            System.out.println("Barang ke-" + (i + 1));
+
+                            System.out.print("Masukkan Nama Barang : ");
+                            input.next();
+                            String namaBarang = input.nextLine();
+
+                            System.out.print("Masukkan Jumlah Barang : ");
+                            int jmlBarang = Integer.parseInt(input.nextLine());
+
+                            System.out.print("Masukkan Harga Barang : ");
+                            int hargaBarang = Integer.parseInt(input.nextLine());
+
+                            System.out.print("Masukkan Tanggal : ");
+                            String date = input.nextLine();
+
+                            items[itemcount][0] = namaBarang;
+                            items[itemcount][1] = date;
+                            items[itemcount][2] = String.valueOf(jmlBarang);
+                            items[itemcount][3] = String.valueOf(hargaBarang);
+                            itemcount++;
+                        }
+                        System.out.println(jml + " barang Berhasil di tambahkan");
+                    } else if (!sudahMasuk) {
+                        System.out.println("Login terlebih dahulu");
+                    }
+                    break;
+                case 4:
+
+                case 5:
+                    if (sudahMasuk) {
                         System.exit(0);
                     } else if (!sudahMasuk) {
                         System.out.println("Login terlebih dahulu");
@@ -70,3 +106,8 @@ public class Main {
 
     }
 }
+// for (String[] i : users) {
+// for (String j : i) {
+// System.out.println(j);
+// }
+// }
